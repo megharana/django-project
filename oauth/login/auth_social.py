e50241b174122074c02ce3514f1a6e4ab33785ae
megharana <megharanait95@gmail.com>
2018-07-14 07:34:37 +0530

    fetching user detail

--- oauth/login/auth_social.py
+++ oauth/login/auth_social.py
@@ -7,7 +7,7 @@ def profile(request):
 	from social_core.backends.github import GithubOAuth2
 	from github import Github
 	g = Github("fbaaaa43e58016f57e9a66ef9397d9a6b44f425b")
-	u = g.search_users(query='abhishekg78')
-	second_repo = u
+	u = g.search_users("abhishek", location="India")[1]
+	second_repo = u.id
 	return render(request,'auth_profile.html',{'data':second_repo})
 	
