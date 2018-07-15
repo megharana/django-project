c4c34ddd7ef09f33e5221bb42e81ab08c90196c3
megharana <megharanait95@gmail.com>
2018-07-15 17:46:16 +0530

    Users are listed working on POST

--- oauth/login/auth_social.py
+++ oauth/login/auth_social.py
@@ -6,8 +6,9 @@ def home(request,*args,**kwargs):
 def profile(request):
 	from social_core.backends.github import GithubOAuth2
 	from github import Github
-	g = Github("fbaaaa43e58016f57e9a66ef9397d9a6b44f425b")
-	u = g.search_users("abhishek", location="India")[1]
-	second_repo = u.id
+	g = Github("6a492d69f880a6e9fb59044c4321e182555466b1")
+	
+	u = g.search_users("Megha", location="India")[0:10]
+	second_repo = u
 	return render(request,'auth_profile.html',{'data':second_repo})
 	
