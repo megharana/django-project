5edda11c4aa4b7b50afa4f58017cfc1a4fcb5cf6
megharana <megharanait95@gmail.com>
2018-07-17 23:29:22 +0530

    Rest framework, tables amde using materialize

--- oauth/login/models.py
+++ oauth/login/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class User(models.Model):
+	username = models.CharField(max_length=30)
+	usertype = models.CharField(max_length=10)
+
+
+	def __str__(self):
+		return self.username
--- oauth/login/views.py
+++ oauth/login/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from social_core.backends.github import GithubOAuth2
 from github import Github
 from login.forms import HomeForm
-
+from login.models import User
 
 def index(request):
 	return HttpResponse("<h1>This is login page</h1>")
 def home(request,*args,**kwargs):
-	
+	all_users=User.objects.all()
+	for user in all_users:
+		print (user.username, user.usertype)
 	return render(request,'auth_social.html')
 
 def getName(request):
@@ -17,9 +19,12 @@ def getName(request):
 		if form.is_valid():
 			name=form.cleaned_data['name']
 			print(name)
-			g = Github("5e3501899d3db07ba1603cc6c19726532d94f820")
-			u = g.search_users(name, location="India")[0:10]
-			return render(request,'auth_profile.html',{'data':u})
+			g = Github("873cf040ccddfa9ed50d579b49abf2b963055e08")
+			users = g.search_users(name, location="India")[0:10]
+			# for user in users:
+			# 	u=User.objects.create(username=user.login, usertype=user.type)
+			# 	u.save()
+			return render(request,'auth_profile.html',{'data':users})
 
 	else:
 			form=HomeForm()
