89ed614085595114516cb2bb480981cd2426735b
megharana <megharanait95@gmail.com>
2018-07-19 02:39:14 +0530

    designing first page,working on save button

--- oauth/oauth/settings.py
+++ oauth/oauth/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'oauth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/oauth'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
