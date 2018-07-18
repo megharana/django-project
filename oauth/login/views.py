34980278cd78c56cf3381115258fe77966f7c514
megharana <megharanait95@gmail.com>
2018-07-19 04:23:18 +0530

    Handling Post Request(Save Button), issue persists

--- oauth/login/views.py
+++ oauth/login/views.py
@@ -14,13 +14,13 @@ def home(request,*args,**kwargs):
 	return render(request,'auth_social.html')
 
 def getName(request):
-	if request.method == 'POST':
+	if request.method == 'POST' and 'name' in request.POST:
 		form1 = HomeForm(request.POST)
 		#form2 = HomeForm(request.POST,prefix="loginInfo")
 		if form1.is_valid():
 			name=form1.cleaned_data['name']
 			print(name)
-			g = Github("873cf040ccddfa9ed50d579b49abf2b963055e08")
+			g = Github("4c41ba93ee379dba07d5e84ae8b8f44d5a14dffc")
 			users = g.search_users(name, location="India")[0:10]
 			# for user in users:
 			# 	u=User.objects.create(username=user.login, usertype=user.type)
@@ -33,4 +33,15 @@ def getName(request):
 	else:
 			form1 = HomeForm()
 			
+	
+
+	if request.method=='POST' and 'loginInfo' in request.POST:
+		form2 = HomeForm(request.POST)
+		if form2.is_valid():
+			loginInfo=form2.cleaned_data['loginInfo']
+			print(loginInfo)
+	else:
+		form2 = HomeForm()
+
+
 	return render(request, 'auth_profile.html', {'form1': form1})
